ed5f655b08eb408a7d99ff1ff13be7c27f6e5dbc
CI00793 <CI00793@MTAA69749>
2017-11-30 13:43:05 -0500

    Add project files.

---
+++ TEKVENUE/Controllers/PortfolioController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+
+namespace TEKVENUE.Controllers
+{
+    
+    
+    public class PortfolioController : Controller
+    {
+        
+        public ActionResult OneColumn()
+        {
+            return View();
+        }
+        
+        public ActionResult TwoColumn()
+        {
+            return View();
+        }
+        
+        public ActionResult SingleItem()
+        {
+            return View();
+        }
+    }
+}
